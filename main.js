c34fbe7fdce1614e81596a8dd7451920062d8e38
Rostrv <trovatorosario91@gmail.com>
2022-04-07 13:44:16 +0200

    minor changes

--- main.js
+++ main.js
@@ -4,19 +4,6 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta,
 i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, 
 il software dice quanti e quali dei numeri da indovinare sono stati individuati.
--document.queryselector() o getelementbyid
--div nell'html
--array numeri random
--Math.random()
-while
-if
-array.includes
-inner.HTML
-setTimeout(funzione,millisecondi)
-ciclo for
-prompt
-parseInt
-
 */
 
 //selezionare elemento dove inserire i numeri
